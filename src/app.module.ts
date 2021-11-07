ef016c337f387b477571f1ef0095f0b698a043e7
Felipe <souzamarquesfelipe@gmail.com>
2021-11-07 20:59:12 -0300

    fixed cors issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { Cloudinary } from "./cloudinary";
 import { ConfigModule } from "@nestjs/config";
 import { UsersModule } from "./users/users.module";
 import { DeckStatsModule } from "./deck-stats/deck-stats.module";
-import { CardStatsModule } from './card-stats/card-stats.module';
+import { CardStatsModule } from "./card-stats/card-stats.module";
 
 @Module({
   imports: [
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "*",
+    origin: "http://localhost:3001, https://kyoka.vercel.app/",
     methods: "GET, PUT, PATCH, POST, DELETE",
     allowedHeaders: "Content-Type, Authorization",
   });
