851ad583396a826afb09e8462ebac72cbe152657
Felipe <souzamarquesfelipe@gmail.com>
2021-11-07 21:28:15 -0300

    heroku integration

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
     allowedHeaders: "Content-Type, Authorization",
   }); */
   app.enableCors();
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
