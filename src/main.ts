2906311ce35688b01fbc18fb87a2ce9dfdb86c03
Felipe <souzamarquesfelipe@gmail.com>
2021-11-07 21:12:50 -0300

    cors issue

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
+  /*   app.enableCors({
     origin: "http://localhost:3001, https://kyoka.vercel.app/",
     methods: "GET, PUT, PATCH, POST, DELETE",
     allowedHeaders: "Content-Type, Authorization",
-  });
+  }); */
+  app.enableCors();
   await app.listen(3001);
 }
 bootstrap();
